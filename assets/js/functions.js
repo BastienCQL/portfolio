6a4ce905fac152b93cbe21ede2d27be73a84c46d
DESKTOP-PHVF9JL\Bastien <bastien.cauquil@gmail.com>
2016-11-04 14:30:17 +0100

    Add animations Fix IE Re-organized .works section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -39,3 +39,36 @@ function clickScroll(){
         });
     }
 }
+
+//This function is a bug fix, the buf encountered was shaky background at scroll on IE
+//Solution found here : http://stackoverflow.com/questions/27966735/why-background-image-is-moving-when-scroll-at-ie
+function fixIe() {
+  if(navigator.userAgent.match(/Trident\/7\./)) {
+     $('body').on("mousewheel", function () {
+         event.preventDefault();
+
+         var wheelDelta = event.wheelDelta;
+
+         var currentScrollPosition = window.pageYOffset;
+         window.scrollTo(0, currentScrollPosition - wheelDelta);
+     });
+
+     $('body').keydown(function (e) {
+         e.preventDefault(); // prevent the default action (scroll / move caret)
+         var currentScrollPosition = window.pageYOffset;
+
+         switch (e.which) {
+
+             case 38: // up
+                 window.scrollTo(0, currentScrollPosition - 120);
+                 break;
+
+             case 40: // down
+                 window.scrollTo(0, currentScrollPosition + 120);
+                 break;
+
+             default: return; // exit this handler for other keys
+         }
+     });
+  }
+}
--- js/common.js
+++ js/common.js
@@ -1,1 +1,79 @@
-function runScroll(){var a=$(".fade");if(a.length){var b=$(".header-span"),c=b.offset().top,c=c+b.height();$(window).scroll(function(){var b=$(this);b.scrollTop()>c?a.addClass("hide"):a.removeClass("hide")})}}function clickScroll(){var a=$(".js-scroll");if(a.length){var b=500;a.on("click",function(a){var c=$(this).attr("href"),d=$(c).offset().top;$("html, body").animate({scrollTop:d},b),a.preventDefault()})}}$(document).ready(function(){runScroll(),clickScroll()});
+$(document).ready(function(){
+    runScroll();
+    clickScroll();
+    fixIe();
+});
+;function runScroll(){
+    var $el = $('.fade'); // Declare the element //
+
+    if($el.length){ // Check if element exists //
+        // Declare our variables //
+        var $header   = $('.header-span'),
+            offsetTop = $header.offset().top, // Get offset from top of page //
+            offsetTop = offsetTop + $header.height(); // Add height of element //
+
+        // Run stuff //
+        $(window).scroll(function() {
+            var $this = $(this);
+
+            // Testing where we are with offsetTop //
+            if ($this.scrollTop() > offsetTop){
+                $el.addClass("hide");
+            } else {
+                $el.removeClass("hide");
+            }
+        });
+    }
+}
+
+function clickScroll(){
+    var $el = $('.js-scroll');
+
+    if($el.length){
+        var speed = 500;
+
+        $el.on('click', function(e) {
+            var page = $(this).attr('href'),
+                offsetHeight = $(page).offset().top;
+
+            $('html, body').animate({
+                scrollTop: offsetHeight
+            }, speed );
+
+            e.preventDefault();
+        });
+    }
+}
+
+//This function is a bug fix, the buf encountered was shaky background at scroll on IE
+//Solution found here : http://stackoverflow.com/questions/27966735/why-background-image-is-moving-when-scroll-at-ie
+function fixIe() {
+  if(navigator.userAgent.match(/Trident\/7\./)) {
+     $('body').on("mousewheel", function () {
+         event.preventDefault();
+
+         var wheelDelta = event.wheelDelta;
+
+         var currentScrollPosition = window.pageYOffset;
+         window.scrollTo(0, currentScrollPosition - wheelDelta);
+     });
+
+     $('body').keydown(function (e) {
+         e.preventDefault(); // prevent the default action (scroll / move caret)
+         var currentScrollPosition = window.pageYOffset;
+
+         switch (e.which) {
+
+             case 38: // up
+                 window.scrollTo(0, currentScrollPosition - 120);
+                 break;
+
+             case 40: // down
+                 window.scrollTo(0, currentScrollPosition + 120);
+                 break;
+
+             default: return; // exit this handler for other keys
+         }
+     });
+  }
+}
